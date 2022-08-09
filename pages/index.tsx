89289b02fe5317815c32479f526a72b0a55c7bfa
ktgnova <hiktgnova@gmail.com>
2022-08-08 23:21:32 -0500

    Create pages layouts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import	React, {ReactElement}	from	'react';
 import	{motion}				from	'framer-motion';
+import	Image					from	'next/image';
 
 const variants = {
 	enter: {
@@ -16,8 +17,6 @@ const variants = {
 function	Content({
 	title = '',
 	description = '',
-	srcScreen = '',
-	srcPrint = '',
 	children = <div />
 }): ReactElement {
 	return (
@@ -29,9 +28,23 @@ function	Content({
 			<h3 className={'text-xl font-bold text-neutral-900'}>{title}</h3>
 			<p className={'mt-4 whitespace-pre-line text-neutral-500'}>{description}</p>
 			{children}
+		</motion.div>
+	);
+}
+
+function    DownloadButtons({
+	srcScreen = '',
+	srcPrint = '',
+	srcPng = ''
+}): ReactElement {
+	return (
+		<motion.div
+			initial={'initial'}
+			whileInView={'enter'}>
 			<div className={'flex flex-row items-center space-x-6'}>
 				<a href={srcScreen} download className={'h-10 w-[184px] border border-primary py-2 text-center font-bold text-primary'}>{'Download for Screen'}</a>
 				<a href={srcPrint} download className={'h-10 w-[184px] border border-primary py-2 text-center font-bold text-primary'}>{'Download for Print'}</a>
+				<a href={srcPng} download className={'h-10 w-[184px] border border-primary py-2 text-center font-bold text-primary'}>{'Download PNG'}</a>
 			</div>
 		</motion.div>
 	);
@@ -46,56 +59,226 @@ function	Index(): ReactElement {
 			<div className={'grid grid-cols-1 gap-x-20 gap-y-14 pb-20 md:grid-cols-2'}>
 				<Content
 					title={'Primary Logo'}
-					description={'Our default, full-color landscape logo only ever appears\non white background.'}
-					srcScreen={'/blue-pill.jpg'}
-					srcPrint={'/blue-pill.jpg'}>
-					<div className={'my-6 h-[315px] border border-neutral-400 bg-white'}>
-
+					description={'Our default, full-color landscape logo only ever appears\non white background.'}>
+					<div className={'my-6 h-[315px]'}>
+						<Image
+							objectFit={'contain'}
+							src={'/index/logo_primary.svg'}
+							width={560}
+							height={316}
+							quality={90}
+							loading={'eager'}
+							priority />
 					</div>
+					<DownloadButtons					
+						srcScreen={'/downloads/YEARN_LOGO_BLUE_RGB.svg'}
+						srcPrint={'/downloads/YEARN_LOGO_BLUE_CMYK.eps'}>
+					</DownloadButtons>
 				</Content>
 				<div />
 
 
 				<Content
 					title={'White logo on dark background'}
-					description={'In cases where the logo appears on a dark background,\nuse the white logo version.'}
-					srcScreen={'/blue-pill.jpg'}
-					srcPrint={'/blue-pill.jpg'}>
-					<div className={'my-6 h-[315px] border border-black bg-black'}>
-
+					description={'In cases where the logo appears on a dark background,\nuse the white logo version.'}>
+					<div className={'my-6 h-[315px]'}>
+						<Image
+							objectFit={'contain'}
+							src={'/index/logo_black_background.svg'}
+							width={560}
+							height={316}
+							quality={90}
+							loading={'eager'}
+							priority />
 					</div>
+					<DownloadButtons
+						srcScreen={'/downloads/YEARN_LOGO_WHITE_RGB.svg'}
+						srcPrint={'downloads/YEARN_LOGO_WHITE_CMYK.eps'}>
+					</DownloadButtons>
 				</Content>
 				<Content
 					title={'White logo on dark background'}
-					description={'In cases where the logo cannot be printed in full colour\nuse the black logo version.'}
-					srcScreen={'/blue-pill.jpg'}
-					srcPrint={'/blue-pill.jpg'}>
-					<div className={'my-6 h-[315px] border border-neutral-400 bg-white'}>
-
+					description={'In cases where the logo cannot be printed in full colour\nuse the black logo version.'}>
+					<div className={'my-6 h-[315px]'}>
+						<Image
+							objectFit={'contain'}
+							src={'/index/logo_white_background.svg'}
+							width={560}
+							height={316}
+							quality={90}
+							loading={'eager'}
+							priority />
 					</div>
+					<DownloadButtons
+						srcScreen={'/downloads/YEARN_LOGO_BLACK_RGB.svg'}
+						srcPrint={'/downloads/YEARN_LOGO_BLACK_CMYK.eps'}>
+					</DownloadButtons>
 				</Content>
 
 
 				<Content
 					title={'Clearspace'}
-					description={'Our logo should always be prominent and legible. The clear space exists to prevent other elements from being placed too close.\nClear space is equal to x1 Yearn symbol.'}
-					srcScreen={'/blue-pill.jpg'}
-					srcPrint={'/blue-pill.jpg'}>
-					<div className={'my-6 h-[315px] border border-neutral-400 bg-white'}>
-
+					description={'Our logo should always be prominent and legible. The clear space exists to prevent other elements from being placed too close.\nClear space is equal to x1 Yearn symbol.'}>
+					<div className={'my-6 h-[315px]'}>
+						<Image
+							objectFit={'contain'}
+							src={'/index/logo_clearspace.svg'}
+							width={560}
+							height={316}
+							quality={90}
+							loading={'eager'}
+							priority />
 					</div>
 				</Content>
 				<Content
 					title={'Minimum size'}
-					description={'The minimum recommended size of the logo is 85 pixels wide (on screen) or 30mm wide (in print).\n​'}
-					srcScreen={'/blue-pill.jpg'}
-					srcPrint={'/blue-pill.jpg'}>
-					<div className={'my-6 h-[315px] border border-neutral-400 bg-white'}>
-
+					description={'The minimum recommended size of the logo is 85 pixels wide (on screen) or 30mm wide (in print).\n​'}>
+					<div className={'my-6 h-[315px]'}>
+						<Image
+							objectFit={'contain'}
+							src={'/index/logo_minimum_size.svg'}
+							width={560}
+							height={316}
+							quality={90}
+							loading={'eager'}
+							priority />
 					</div>
 				</Content>
 
 			</div>
+			<h2 className={'mb-10 text-3xl font-bold text-neutral-900'}>
+				{'Symbol'}
+			</h2>
+			<div className={'grid grid-cols-1 gap-x-20 gap-y-14 pb-20 md:grid-cols-2'}>
+				<Content
+					title={'Primary Symbol'}
+					description={'Our default, full-color symbol only ever appears\non white background.'}>
+					<div className={'my-6 h-[315px]'}>
+						<Image
+							objectFit={'contain'}
+							src={'/index/symbol.svg'}
+							width={560}
+							height={316}
+							quality={90}
+							loading={'eager'}
+							priority />
+					</div>
+					<DownloadButtons
+						srcScreen={'/downloads/YEARN_SYMBOL_BLUE_RGB.svg'}
+						srcPrint={'/downloads/YEARN_SYMBOL_BLUE_CMYK.eps'}>
+					</DownloadButtons>
+				</Content>
+				<div />
+
+
+				<Content
+					title={'White symbol on dark background'}
+					description={'In cases where the symbol appears on a dark background,\nuse the white symbol version.'}>
+					<div className={'my-6 h-[315px]'}>
+						<Image
+							objectFit={'contain'}
+							src={'/index/symbol_black_background.svg'}
+							width={560}
+							height={316}
+							quality={90}
+							loading={'eager'}
+							priority />
+					</div>
+					<DownloadButtons
+						srcScreen={'/downloads/YEARN_SYMBOL_WHITE_RGB.svg'}
+						srcPrint={'/downloads/YEARN_SYMBOL_WHITE_CMYK.eps'}>
+					</DownloadButtons>
+				</Content>
+				<Content
+					title={'Black symbol on white background'}
+					description={'In cases where the symbol cannot be printed in full colour\nuse the black symbol version.'}>
+					<div className={'my-6 h-[315px] bg-white'}>
+						<Image
+							objectFit={'contain'}
+							src={'/index/symbol_white_background.svg'}
+							width={560}
+							height={316}
+							quality={90}
+							loading={'eager'}
+							priority />
+					</div>
+					<DownloadButtons
+						srcScreen={'/downloads/YEARN_SYMBOL_BLACK_RGB.svg'}
+						srcPrint={'/downloads/YEARN_SYMBOL_BLACK_CMYK.svg'}>
+					</DownloadButtons>
+				</Content>
+
+
+				<Content
+					title={'Clearspace'}
+					description={'Our symbol should always be prominent and legible. The clear space exists to prevent other elements from being placed too close.\nClear space is equal to x1 Yearn symbol.'}>
+					<div className={'my-6 h-[315px] bg-white'}>
+						<Image
+							objectFit={'cover'}
+							src={'/index/symbol_clearspace.svg'}
+							width={560}
+							height={316}
+							quality={90}
+							loading={'eager'}
+							priority />
+					</div>
+				</Content>
+				<Content
+					title={'Minimum size'}
+					description={'The minimum recommended size of the symbol is 40 pixels wide (on screen) or 15mm wide (in print).\n\n'}>
+					<div className={'my-6 h-[315px] bg-white'}>
+						<Image
+							objectFit={'cover'}
+							src={'/index/symbol_minimum_size.svg'}
+							width={560}
+							height={316}
+							quality={90}
+							loading={'eager'}
+							priority />
+					</div>
+				</Content>
+	
+
+			</div>
+			<h2 className={'mb-10 text-3xl font-bold text-neutral-900'}>
+				{'Token symbol'}
+			</h2>
+			<div className={'grid grid-cols-1 gap-x-20 gap-y-14 pb-20 md:grid-cols-2'}>
+				<Content
+					title={'Token'}
+					description={'Our full-color token symbol can also be used as our social avatar.'}>
+					<div className={'my-6 h-[315px] bg-white'}>
+						<Image
+							objectFit={'cover'}
+							src={'/index/token_symbol.svg'}
+							width={560}
+							height={316}
+							quality={90}
+							loading={'eager'}
+							priority />
+					</div>
+					<DownloadButtons
+						srcScreen={'/downloads/YEARN_TOKEN_BLUE_RGB.svg'}>
+					</DownloadButtons>
+				</Content>
+				<Content
+					title={''}
+					description={''}>
+					<div className={'mt-20 mb-6 h-[315px] bg-white'}>
+						<Image
+							objectFit={'cover'}
+							src={'/index/token_symbol_grid.svg'}
+							width={560}
+							height={316}
+							quality={90}
+							loading={'eager'}
+							priority />
+					</div>
+				</Content>
+
+
+			</div>
+
 		</section>
 	);
 }
--- pages/templates.tsx
+++ pages/templates.tsx
@@ -1,51 +1,68 @@
 import	React, {ReactElement}	from	'react';
-import	{Card, Banner, DescriptionList}			from	'@yearn-finance/web-lib/components';
+import	{motion}				from	'framer-motion';
+import	Image					from	'next/image';
 
-function	Content(): ReactElement {
+
+const variants = {
+	enter: {
+		y: 0,
+		opacity: 1,
+		transition: {
+			duration: 0.5,
+			ease: 'linear'
+		}
+	},
+	initial: {y: 60, opacity: 0}
+};
+
+function	Content({
+	title = '',
+	description = '',
+	srcScreen = '',
+	children = <div />
+}): ReactElement {
 	return (
-		<div className={'w-full'}>
-			<div className={'flex w-full flex-col gap-2'}>
-				<h4 className={'mb-2'}>{'How to use'}</h4>
-				<div className={'mb-6 space-y-2'}>
-					<p className={'text-neutral-500'}>
-						{'The web-lib contains components that can be used to build a new web application (website, dashboard, standalone, etc.), focused for the needs of Yearn Finance and the specificities of the web-3 ecosystem.'}
-					</p>
-					<p className={'text-neutral-500'}>
-						{'The Lib is divided in various sub-sections: components, layouts, utils, contexts and hooks. With it, you should have everything you need to start working with Yearn and Ethereum, from the "Connect Wallet" to the designs of the buttons.'}
-					</p>
-					<p className={'text-neutral-500'}>
-						{'You can now start playing by editing the `pages/index.tsx` file.'}
-					</p>
-				</div>
-				<Card variant={'background'}>
-					<DescriptionList
-						options={[
-							{title: 'Web-Lib Repo', details: 'https://web.ycorpo.com/'}, 
-							{title: 'NextJs documentation', details: 'https://nextjs.org/'}
-						]} />
-				</Card>
+		<motion.div
+			initial={'initial'}
+			whileInView={'enter'}
+			className={'flex flex-col'}
+			variants={variants}>
+			<h3 className={'text-xl font-bold text-neutral-900'}>{title}</h3>
+			<p className={'mt-4 whitespace-pre-line text-neutral-500'}>{description}</p>
+			{children}
+			<div className={'flex flex-row items-center space-x-6'}>
+				<a href={srcScreen} download className={'h-10 w-[184px] border border-primary py-2 text-center font-bold text-primary'}>{'Download for Screen'}</a>
 			</div>
-		</div>
+		</motion.div>
 	);
 }
 
-function	Index(): ReactElement {
+function	Templates(): ReactElement {
 	return (
-		<section aria-label={'some default section'}>
-			<div className={'mb-4'}>
-				<Banner title={'Yearn Finance'}>
-					<div className={'space-y-4'}>
-						<p className={'text-primary-500'}>{'Yearn strategists and systems identify the optimal opportunities for yield in the market. Each Vault auto-compounds earned tokens, meaning Yearn reinvests earned tokens to generate additional earnings over time.'}</p>
-						<p className={'text-primary-500'}>{'Vaults are a passive investing strategy, enabling people to put their capital to work via automation.'}</p>
-						<p className={'text-primary-500'}>{'Enjoy the growth market!'}</p>
+		<section aria-label={'Templates'}>
+			<h2 className={'mb-6 text-3xl font-bold text-neutral-900'}>
+				{'Templates'}
+			</h2>
+			<h3 className={'font-bold'}>
+				{'Twitter'}
+			</h3>
+			<div className={'grid grid-cols-1 gap-x-20 pb-20'}>
+				<Content>
+					<div className={'my-6 h-[680px]'}>
+						<Image
+							objectFit={'contain'}
+							src={'/templates/twitter.jpg'}
+							width={1200}
+							height={680}
+							quality={90}
+							loading={'eager'}
+							priority />
 					</div>
-				</Banner>
+				</Content>
+
 			</div>
-			<Card>
-				<Content />
-			</Card>
 		</section>
 	);
 }
 
-export default Index;
+export default Templates;
