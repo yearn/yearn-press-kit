2cf606a9880b54da08ae52dc5d3a71ba957a1eae
ktgnova <hiktgnova@gmail.com>
2022-08-16 09:59:44 -0500

    Download buttons to a separate component

---
+++ components/DownloadButtons.tsx
@@ -0,0 +1,22 @@
+import	React, {ReactElement}	            from	'react';
+import	{motion}							from	'framer-motion';
+
+function    DownloadButtons({
+	srcScreen = '',
+	srcPrint = '',
+	srcPng = ''
+}): ReactElement {
+	return (
+		<motion.div
+			initial={'initial'}
+			whileInView={'enter'}>
+			<div className={'flex flex-col items-center space-y-4 lg:flex-row lg:space-x-6 lg:space-y-0'}>
+				<a href={srcScreen} download className={'h-10 w-full border border-primary py-2 text-center font-bold text-primary hover:bg-light-blue lg:w-[184px] lg:pt-3 lg:text-xs xl:py-2 xl:text-base'}>{'Download for Screen'}</a>
+				<a href={srcPrint} download className={'h-10 w-full border border-primary py-2 text-center font-bold text-primary hover:bg-light-blue lg:w-[184px] lg:pt-3 lg:text-xs xl:py-2 xl:text-base'}>{'Download for Print'}</a>
+				<a href={srcPng} download className={'h-10 w-full border border-primary py-2 text-center font-bold text-primary hover:bg-light-blue lg:w-[184px] lg:pt-3 lg:text-xs xl:py-2 xl:text-base'}>{'Download PNG'}</a>
+			</div>
+		</motion.div>
+	);
+}
+
+export default DownloadButtons;
