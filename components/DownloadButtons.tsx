acaa04425efc39aace102e300d52d99d9964467a
Majorfi <major-tom.eth@pm.me>
2022-08-16 17:31:38 +0200

    fix: color classnames

--- components/DownloadButtons.tsx
+++ components/DownloadButtons.tsx
@@ -11,9 +11,9 @@ function    DownloadButtons({
 			initial={'initial'}
 			whileInView={'enter'}>
 			<div className={'flex flex-col items-center space-y-4 lg:flex-row lg:space-x-6 lg:space-y-0'}>
-				<a href={srcScreen} download className={'h-10 w-full border border-primary py-2 text-center font-bold text-primary hover:bg-light-blue lg:w-[184px] lg:pt-3 lg:text-xs xl:py-2 xl:text-base'}>{'Download for Screen'}</a>
-				<a href={srcPrint} download className={'h-10 w-full border border-primary py-2 text-center font-bold text-primary hover:bg-light-blue lg:w-[184px] lg:pt-3 lg:text-xs xl:py-2 xl:text-base'}>{'Download for Print'}</a>
-				<a href={srcPng} download className={'h-10 w-full border border-primary py-2 text-center font-bold text-primary hover:bg-light-blue lg:w-[184px] lg:pt-3 lg:text-xs xl:py-2 xl:text-base'}>{'Download PNG'}</a>
+				<a href={srcScreen} download className={'border-primary text-primary h-10 w-full border py-2 text-center font-bold hover:bg-light-blue lg:w-[184px] lg:pt-3 lg:text-xs xl:py-2 xl:text-base'}>{'Download for Screen'}</a>
+				<a href={srcPrint} download className={'border-primary text-primary h-10 w-full border py-2 text-center font-bold hover:bg-light-blue lg:w-[184px] lg:pt-3 lg:text-xs xl:py-2 xl:text-base'}>{'Download for Print'}</a>
+				<a href={srcPng} download className={'border-primary text-primary h-10 w-full border py-2 text-center font-bold hover:bg-light-blue lg:w-[184px] lg:pt-3 lg:text-xs xl:py-2 xl:text-base'}>{'Download PNG'}</a>
 			</div>
 		</motion.div>
 	);
